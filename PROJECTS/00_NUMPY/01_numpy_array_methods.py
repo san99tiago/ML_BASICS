7236b2adc5a1b2d21a74383028eeef7f049b6e8e
san99tiago <san99tiago@gmail.com>
2020-06-25 05:07:49 -0500

    Added useful methods for working with numpy arrays

--- PROJECTS/00_NUMPY/01_numpy_array_methods.py
+++ PROJECTS/00_NUMPY/01_numpy_array_methods.py
@@ -1,4 +1,4 @@
-#METHODS AND UTILITIES OF THE NUMPY ARRAYS
+#USEFUL METHODS OF THE NUMPY ARRAYS
 #Santiago Garcia Arango
 #-------------------------------------------------------------------------
 import numpy as np
@@ -9,9 +9,31 @@ array_2 = np.random.randint(0,50,10) #Vector of 5 random integers(0,50)
 print("array_1:\n", array_1, "\n")
 print("array_2:\n", array_2, "\n")
 
+
 #----------------------RESHAPE METHOD--------------------------------------
 #This method allows us to reshape an array into a given shape (must match)
+#Remark: the product of dimensions N*M must be equal to the size of vector
 new_array_1 = array_1.reshape(5,5)
-print("new_array_1:\n", new_array_1, "\n")
+print("array_1 (reshaped):\n", new_array_1, "\n")
 
 
+#-------------------------GET SHAPE OF ARRAY------------------------------
+print("SHAPE(array_1) -->", array_1.shape )
+print("SHAPE(array_1 (reshaped) ) -->", new_array_1.shape )
+print("SHAPE(array_2) -->", array_2.shape , "\n")
+
+
+
+#-------------------MAXIMUM AND MINIMUM OF ARRAY---------------------------
+# array.max(): returns the max VALUE of a given array
+# array.min(): returns the min VALUE of a given array
+# array.argmax(): returns INDEX location of the max VALUE of given array
+# array.argmin(): returns INDEX location of the min VALUE of given array
+print("MAX(array_2) =", array_2.max(), " (at index", array_2.argmax(), ")\n")
+print("MIN(array_2) =", array_2.min(), " (at index", array_2.argmin(), ")\n")
+
+
+
+#------------------------DATATYPE OF ARRAY----------------------------------
+print("DATATYPE(array_1) -->", array_1.dtype )
+print("DATATYPE(array_2) -->", array_2.dtype )
