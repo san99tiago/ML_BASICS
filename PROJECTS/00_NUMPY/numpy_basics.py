db43b274118b1570ff6e7f0d5deabf393cb697e9
san99tiago <san99tiago@gmail.com>
2020-06-25 03:14:47 -0500

    Created first numpy file

---
+++ PROJECTS/00_NUMPY/numpy_basics.py
@@ -0,0 +1,14 @@
+#BASIC UNDERSTANDING OF NUMPY
+#Santiago Garcia Arango
+#-------------------------------------------------------------------------
+#NumPy is a linear algebra library for Python. It is used in many
+#other libraries for the "data-ecosystem".
+#It is created based on Low-level C programming, making it fast!
+
+#For using NumPy, we have to install it first (with pip package installer)
+#--------------------------------------------------------------------------
+
+
+
+
+
