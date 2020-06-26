e07150b7d46a70b10ca27b93666f2f3c0823dfef
san99tiago <san99tiago@gmail.com>
2020-06-25 19:19:57 -0500

    Creation of numpy manipulations file

---
+++ PROJECTS/00_NUMPY/03_numpy_manipularions.py
@@ -0,0 +1,7 @@
+#NUMPY MANIPULATIONS OF ARRAYS
+#Santiago Garcia Arango
+#-------------------------------------------------------------------------
+import numpy as np
+
+my_array = np.arange( 1, 11 ) # [1,2,..,8,9,10]
+print( "my_array=\n", my_array, "\n")
