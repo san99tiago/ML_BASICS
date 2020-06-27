c5d0b7f5f1e78db3e8559e2751f02f29e2041026
san99tiago <san99tiago@gmail.com>
2020-06-26 20:53:14 -0500

    Simple numpy operations with examples

---
+++ PROJECTS/00_NUMPY/04_numpy_operations.py
@@ -0,0 +1,19 @@
+#NUMPY OPERATIONS OF ARRAYS
+#Santiago Garcia Arango
+#-------------------------------------------------------------------------
+import numpy as np
+
+my_array = np.arange(0,11)
+print("my_array = ", my_array, "\n")
+
+#--------------------SCALAR OPERATIONS OF ARRAYS--------------------------
+print("my_array + 100 = ", my_array+100, "\n")
+print("my_array - 100 = ", my_array-100, "\n")
+print("my_array*4 = ", my_array*4, "\n")
+print("my_array/4 = ", my_array/4, "\n")
+
+
+#------------------BASIC OPERATIONS OF ARRAYS-----------------------------
+print("my_array + my_array = ", my_array + my_array, "\n")
+print("my_array - my_array = ", my_array - my_array, "\n")
+
