8390d86393ed25665e9b6c9c8e9d2abdec0eb74e
san99tiago <san99tiago@gmail.com>
2020-07-04 00:34:59 -0500

    Creation of script for Object approach of matplotlib

---
+++ PROJECTS/02_MATPLOTLIB/01_matplotlib.py
@@ -0,0 +1,15 @@
+#BASICS ON MATPLOTLIB LIBRARY
+#Santiago Garcia Arango, July 2020
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#In this script, we will use the Object Oriented approach of...
+#... the matplotlib library. It is easier and gives us more...
+#... control of the different plots and its applications.
+
+
+#We define a figure for the creation of the plot
+figure_1 = plt.figure()
+
+
