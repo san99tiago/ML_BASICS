a3057fecdf279a1aac4d8b03c2c8b3f545ca6ab3
san99tiago <san99tiago@gmail.com>
2020-07-09 16:22:16 -0500

    added simple changes to comments and strucutres

--- 00_LIBRARIES/01_PANDAS/02_pandas_dataframes_2.py
+++ 00_LIBRARIES/01_PANDAS/02_pandas_dataframes_2.py
@@ -1,51 +1,51 @@
-#MULTI-INDEX DATAFRAMES IN PANDAS (AKA: "INDEX-HIGHER KEY" )
-#Santiago Garcia Arango, June 2020
+# MULTI-INDEX DATAFRAMES IN PANDAS (AKA: "INDEX-HIGHER KEY" )
+# Santiago Garcia Arango, June 2020
 
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
 
-#---------------MULTI-INDEX DATAFRAME PANDAS--------------------
-#With Pandas, we can achieve a multi-level indexing layout, where...
-#... the DataFrame has N levels of indexing. This is really usefull...
-#... when working with multiple parameters and sub-parameters of info.
+# ---------------MULTI-INDEX DATAFRAME PANDAS--------------------
+# With Pandas, we can achieve a multi-level indexing layout, where...
+# ... the DataFrame has N levels of indexing. This is really usefull...
+# ... when working with multiple parameters and sub-parameters of info.
 
-#First index level (that will contain the second one)...
-outside = ["G1","G1","G1","G2","G2","G2","G3","G3","G3"] #List 1
+# First index level (that will contain the second one)...
+outside = ["G1", "G1", "G1", "G2", "G2", "G2", "G3", "G3", "G3"]  # List 1
 
-#Second index level (that are "inside" the First index level)...
-inside = [1,2,3,1,2,3,1,2,3] #List 2
-
-
-#We create the "interconnection" of these two index levels...
-#Remark: this must be done in a list of tuples (to work properly)
-h_index = list( zip(outside, inside) ) #List of both lists (in tuples)
-h_index = pd.MultiIndex.from_tuples( h_index ) #Create MultiIndex DF
-print( "h_index =\n", h_index, "\n" )
+# Second index level (that are "inside" the First index level)...
+inside = [1, 2, 3, 1, 2, 3, 1, 2, 3]  # List 2
 
 
-#Now, we can go ahead and create the MultiIndex Dataframe...
-data_frame = pd.DataFrame( np.random.randn(9,3), h_index, ['A','B','C'] )
-print( "data_frame =\n", data_frame, "\n" )
+# We create the "interconnection" of these two index levels...
+# Remark: this must be done in a list of tuples (to work properly)
+h_index = list(zip(outside, inside))  # List of both lists (in tuples)
+h_index = pd.MultiIndex.from_tuples(h_index)  # Create MultiIndex DF
+print("h_index =\n", h_index, "\n")
 
-#Extra/optional step (naming the index-levels)...
+
+# Now, we can go ahead and create the MultiIndex Dataframe...
+data_frame = pd.DataFrame(np.random.randn(9, 3), h_index, ['A', 'B', 'C'])
+print("data_frame =\n", data_frame, "\n")
+
+# Extra/optional step (naming the index-levels)...
 data_frame.index.names = ["Groups", "Numbers"]
-print( "data_frame (with index names) =\n", data_frame, "\n" )
+print("data_frame (with index names) =\n", data_frame, "\n")
 
 
-#-------------SEARCHING FOR MULTI-INDEX DATAFRAMES----------------
-#Searching first index level...
+# -------------SEARCHING FOR MULTI-INDEX DATAFRAMES----------------
+# Searching first index level...
 #  1) First way of doing this (access corresponding index):
 print("data_frame.loc['G1']=\n", data_frame.loc['G1'], "\n")
 #  2) Second way of doing this (access cross_section of index):
 print("data_frame.xs('G1')=\n", data_frame.xs('G1'), "\n")
 
 
-#Searching second index level...
+# Searching second index level...
 print("data_frame.loc['G1'].loc[1]=\n", data_frame.loc['G1'].loc[1], "\n")
 
 
-#Searching general condition for a "N level" index...
-#Note: will search in Any 1st level, only focusing on 2nd level condition
-print("data_frame.xs(2,level='Numbers') =")
-print( data_frame.xs(2,level='Numbers'))
+# Searching general condition for a "N level" index...
+# Note: will search in Any 1st level, only focusing on 2nd level condition
+print("data_frame.xs(2, level='Numbers') =")
+print(data_frame.xs(2, level='Numbers'))
--- 00_LIBRARIES/01_PANDAS/03_pandas_missing_data.py
+++ 00_LIBRARIES/01_PANDAS/03_pandas_missing_data.py
@@ -1,38 +1,36 @@
-#HOW TO HANDLE MISSING DATA IN PANDAS
-#Santiago Garcia Arango, June 2020
+# HOW TO HANDLE MISSING DATA IN PANDAS
+# Santiago Garcia Arango, June 2020
 
 import numpy as np
 import pandas as pd
 
-#Create a dictionary with missing values (to show missing data examples)
+# Create a dictionary with missing values (to show missing data examples)
 dictionary = {
-    "A":[1,2,np.nan,5],
-    "B":[5,np.nan,np.nan,2],
-    "C":[1,2,3,4],
-    "D":[3,np.nan,3,5]
+    "A": [1, 2, np.nan, 5],
+    "B": [5, np.nan, np.nan, 2],
+    "C": [1, 2, 3, 4],
+    "D": [3, np.nan, 3, 5]
     }
 
-#Create data_frame from the dictionary already created
-data_frame = pd.DataFrame( dictionary )
-print( "data_frame=\n", data_frame, "\n")
+# Create data_frame from the dictionary already created
+data_frame = pd.DataFrame(dictionary)
+print("data_frame=\n", data_frame, "\n")
 
-#----------------METHOD 1(delete rows/columns with NAN)----------------------
-#1) Delete rows that have NAN values from the data_frame.
+# ----------------METHOD 1(delete rows/columns with NAN)----------------------
+# 1) Delete rows that have NAN values from the data_frame.
 print("data_frame.dropna() [delete NAN rows]=")
-print( data_frame.dropna(), "\n")
+print(data_frame.dropna(), "\n")
 
-#2) Delete columns that have NAN values from the data_frame.
+# 2) Delete columns that have NAN values from the data_frame.
 print("data_frame.dropna(axis=1) [delete NAN rows]=")
-print( data_frame.dropna(axis=1), "\n")
+print(data_frame.dropna(axis=1), "\n")
 
 
-
-#---------------------------METHOD 2(fill the NAN)--------------------------
-#Get the average(or mean) value of each data_frame column
+# ---------------------------METHOD 2(fill the NAN)--------------------------
+# Get the average(or mean) value of each data_frame column
 average_1 = data_frame.mean()
-print("average_1 (COLUMNS average)=\n", average_1, "\n" )
+print("average_1 (COLUMNS average)=\n", average_1, "\n")
 
-#We can strategically fill the missing values (NAN) with the average value
+# We can strategically fill the missing values (NAN) with the average value
 print("data_frame.fillna(value=average_1)=")
 print(data_frame.fillna(value=average_1), "\n")
-
