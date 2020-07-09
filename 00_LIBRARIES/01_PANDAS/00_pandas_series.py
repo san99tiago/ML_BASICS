53ba6fc0d062c13df5bfb5d84c8e34fc3fc8327a
san99tiago <san99tiago@gmail.com>
2020-07-09 16:16:48 -0500

    Fixed flake8 structures for conventions

--- 00_LIBRARIES/01_PANDAS/00_pandas_series.py
+++ 00_LIBRARIES/01_PANDAS/00_pandas_series.py
@@ -1,27 +1,27 @@
-#SERIES WITH PANDAS
-#Santiago Garcia Arango, June 2020
+# SERIES WITH PANDAS
+# Santiago Garcia Arango, June 2020
 
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
-#----------------UNDERSTANDING SERIES IN PANDAS--------------------------
-#Basic information to play with pandas
+# ----------------UNDERSTANDING SERIES IN PANDAS--------------------------
+# Basic information to play with pandas
 my_labels = ["A", "B", "C"]
 my_data = [10, 20, 30]
-my_array = np.array( my_data )
-my_dict = {"A":10, "B":20, "C":30}
+my_array = np.array(my_data)
+my_dict = {"A": 10, "B": 20, "C": 30}
 print("my_labels = ", my_labels)
 print("my_data = ", my_data)
 print("my_array = ", my_array)
 print("my_dict = ", my_dict, "\n")
 
-#We create different versions of Series, based on the parameters
-#note: we can pass lists, arrays as "data" or even dictionaries
-series_1 = pd.Series( data=my_data )
-series_2 = pd.Series( data=my_array )
-series_3 = pd.Series( data=my_data, index=my_labels)
-series_4 = pd.Series( data=my_array, index=my_labels)
-series_5 = pd.Series( my_dict )
+# We create different versions of Series, based on the parameters
+# note: we can pass lists, arrays as "data" or even dictionaries
+series_1 = pd.Series(data=my_data)
+series_2 = pd.Series(data=my_array)
+series_3 = pd.Series(data=my_data, index=my_labels)
+series_4 = pd.Series(data=my_array, index=my_labels)
+series_5 = pd.Series(my_dict)
 
 print("series_1 =\n", series_1, "\n")
 print("series_2 =\n", series_2, "\n")
@@ -30,15 +30,15 @@ print("series_4 =\n", series_4, "\n")
 print("series_5 =\n", series_5, "\n")
 
 
-#-------------------EXAMPLE WITH SERIES----------------------------------
-#The correct way to pass arguments is (<DATA>, <INDEX>)
-series_countries_1 = pd.Series([1,2,3,4], ["Colombia", "USA", "Francia", "Alemania"])
-series_countries_2 = pd.Series(["Colombia", "USA", "Francia", "Alemania"],[1,2,3,4])
+# -------------------EXAMPLE WITH SERIES----------------------------------
+# The correct way to pass arguments is (<DATA>, <INDEX>)
+series_countries_1 = pd.Series([1, 2, 3, 4], ["COL", "USA", "FRA", "GER"])
+series_countries_2 = pd.Series(["COL", "USA", "FRA", "GER"], [1, 2, 3, 4])
 
 print("series_countries_1 = \n", series_countries_1, "\n")
 print("series_countries_2 = \n", series_countries_2, "\n")
 
 
-#To access the Series, it is similar to a python dictionary
+# To access the Series, it is similar to a python dictionary
 print("series_countries_1['USA'] = ", series_countries_1["USA"], "\n")
 print("series_countries_2[1] = ", series_countries_2[1], "\n")
--- 00_LIBRARIES/01_PANDAS/01_pandas_dataframes.py
+++ 00_LIBRARIES/01_PANDAS/01_pandas_dataframes.py
@@ -1,69 +1,70 @@
-#DATAFRAMES IN PANDAS
-#Santiago Garcia Arango, June 2020
+# DATAFRAMES IN PANDAS
+# Santiago Garcia Arango, June 2020
 
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
-#Create a dataframe based on ( <DATA>, <HEADERS_VERTICAL>, <HEADERS_HORIZONTAL>)
-data_frame = pd.DataFrame( np.random.randn(5,4),["A","B","C","D","E"],["W","X","Y","Z"] )
+# Create dataframe based on ( <DATA>, <HEADERS_VERTICAL>, <HEADERS_HORIZONTAL>)
+data_frame = pd.DataFrame(
+    np.random.randn(5, 4),
+    ["A", "B", "C", "D", "E"],
+    ["W", "X", "Y", "Z"]
+    )
 
 print("data_frame:")
-print( data_frame, "\n")
+print(data_frame, "\n")
 
-#To show a determined column from the DataFrame...(it is actually a Series)
+# To show a determined column from the DataFrame...(it is actually a Series)
 print("data_frame['W'] (COLUMN):")
-print( data_frame['W'], "\n")
+print(data_frame['W'], "\n")
 
-#To show multiple wanted columns from DataFrame...
-print("data_frame[['W','Z']] (COLUMNS):")
-print( data_frame[['W','Z']], "\n")
+# To show multiple wanted columns from DataFrame...
+print("data_frame[['W', 'Z']] (COLUMNS):")
+print(data_frame[['W', 'Z']], "\n")
 
-#To show a determined row from the DataFrame
-#two ways:
+# To show a determined row from the DataFrame
+# two ways:
 # 1) data_frame.loc[<ROW_NAME>]
 # 2) data_frame.iloc[<ROW_INDEX>]
 print("data_frame.loc['A'] (ROW):")
-print( data_frame.loc['A'], "\n")
+print(data_frame.loc['A'], "\n")
 
 
-#Add new column to an existing DataFrame
-data_frame['NEW_COL'] = np.random.randn(5,1)
+# Add new column to an existing DataFrame
+data_frame['NEW_COL'] = np.random.randn(5, 1)
 print("data_frame (new column) :")
-print( data_frame, "\n")
+print(data_frame, "\n")
 
-#To delete column, we can use "data_frame.drop(<COL_NAME>, axis=1)"
-#Note: "axis=1" is to indicate that we want to delete a column.
-#if we want to delete a row, then is "axis=0"
+# To delete column, we can use "data_frame.drop(<COL_NAME>, axis=1)"
+# Note: "axis=1" is to indicate that we want to delete a column.
+# if we want to delete a row, then is "axis=0"
 
-data_frame = data_frame.drop( "Y", axis=1 )
+data_frame = data_frame.drop("Y", axis=1)
 print("data_frame (deleted Y):")
 print(data_frame, "\n")
 
 
-#To show specific value, by the row and column name
-print("data_frame.loc['B','X'] :")
-print( data_frame.loc['B','X'], "\n")
+# To show specific value, by the row and column name
+print("data_frame.loc['B', 'X'] :")
+print(data_frame.loc['B', 'X'], "\n")
 
 
-#To evaluate conditions given in an specific dataframe (returns boolean DF)
+# To evaluate conditions given in an specific dataframe (returns boolean DF)
 print("data_frame > 0 :")
 print(data_frame > 0, "\n")
 
-#To show the dataframe where the condition given is true...
-print("data_frame[ data_frame>0 ] :")
-print(data_frame[ data_frame>0 ], "\n")
+# To show the dataframe where the condition given is true...
+print("data_frame[ data_frame > 0 ] :")
+print(data_frame[data_frame > 0], "\n")
 
 
-#Multiple AND conditions of a dataframe in pandas (NOT with "and" operator)
-#Note: for multiple conditions, we must use "&" instead of "and" !!
-print("data_frame[ (data_frame['W']>0) & (data_frame['X']>0)]:\n")
-print( data_frame[ (data_frame['W']>0) & (data_frame['X']>0) ], "\n" )
+# Multiple AND conditions of a dataframe in pandas (NOT with "and" operator)
+# Note: for multiple conditions, we must use "&" instead of "and" !!
+print("data_frame[ (data_frame['W'] > 0) & (data_frame['X'] > 0)]:\n")
+print(data_frame[(data_frame['W'] > 0) & (data_frame['X'] > 0)], "\n")
 
 
-#Multiple OR conditions of a dataframe in pandas (NOT with "or" operator)
-#Note: for multiple or conditions, we must use "|" instead of "or" !!
-print("data_frame[ (data_frame['W']>0) | (data_frame['X']>0)]:\n")
-print( data_frame[ (data_frame['W']>0) | (data_frame['X']>0) ], "\n")
-
-
-
+# Multiple OR conditions of a dataframe in pandas (NOT with "or" operator)
+# Note: for multiple or conditions, we must use "|" instead of "or" !!
+print("data_frame[ (data_frame['W'] > 0) | (data_frame['X'] > 0)]:\n")
+print(data_frame[(data_frame['W'] > 0) | (data_frame['X'] > 0)], "\n")
