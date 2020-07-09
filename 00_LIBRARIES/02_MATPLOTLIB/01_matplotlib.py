cf3c327ae90dbea9e1656349b823cb08ea23c044
san99tiago <san99tiago@gmail.com>
2020-07-09 16:45:44 -0500

    Changed comments, spaces and structure to fit flake8 standards

--- 00_LIBRARIES/02_MATPLOTLIB/01_matplotlib.py
+++ 00_LIBRARIES/02_MATPLOTLIB/01_matplotlib.py
@@ -1,47 +1,47 @@
-#WORKING WITH OBJECT-ORIENTED APPROACH TO MATPLOTLIB PLOTS
-#Santiago Garcia Arango, July 2020
+# WORKING WITH OBJECT-ORIENTED APPROACH TO MATPLOTLIB PLOTS
+# Santiago Garcia Arango, July 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-#In this script, we will use the Object Oriented approach of...
-#... the matplotlib library. It is easier and gives us more...
-#... control of the different plots and its applications.
+# In this script, we will use the Object Oriented approach of...
+# ... the matplotlib library. It is easier and gives us more...
+# ... control of the different plots and its applications.
 
-#We create the vectors for the plots
-x_1 = np.linspace(0,5,1024)
+# We create the vectors for the plots
+x_1 = np.linspace(0, 5, 1024)
 y_1 = x_1**2
 
-#----------------OBJECT ORIENTED METHOD FOR FIGURES-------------------
-#--->FIRST FIGURE...
-#We define a figure for the creation of the plot
+# ----------------OBJECT ORIENTED METHOD FOR FIGURES-------------------
+# --->FIRST FIGURE...
+# We define a figure for the creation of the plot
 figure_1 = plt.figure(1)
 
-#We create the axes that make possible the plots (like in MATLAB)
-# Note: fig.add_axes( self, *args, **kwargs), where:
-# rect           --> [LEFT,BOTTOM,WIDTH,HEIGHT] (in percentages)
-# projection     --> projection type of the Axes.
-# sharex,sharey  --> Parameters that share the "x" or "y" axis.
-# label          --> Label for the returned axes.
+# We create the axes that make possible the plots (like in MATLAB)
+#  Note: fig.add_axes( self, *args, **kwargs), where:
+#  rect           --> [LEFT,BOTTOM,WIDTH,HEIGHT] (in percentages)
+#  projection     --> projection type of the Axes.
+#  sharex,sharey  --> Parameters that share the "x" or "y" axis.
+#  label          --> Label for the returned axes.
 
-axes_1 = figure_1.add_axes([0.1,0.1,0.8,0.8])
+axes_1 = figure_1.add_axes([0.1, 0.1, 0.8, 0.8])
 
-#We generate the plot to the figure_1 and its axes.
-axes_1.plot( x_1, y_1, c='b', linewidth=3 )
+# We generate the plot to the figure_1 and its axes.
+axes_1.plot(x_1, y_1, c='b', linewidth=3)
 
-#We customize figure_1 with title, "x"-"y" labels
+# We customize figure_1 with title, "x"-"y" labels
 axes_1.set_title("MY COOL TITLE FOR FIGURE 1")
 axes_1.set_xlabel("MY  \"X\"  LABEL")
 axes_1.set_ylabel("MY  \"Y\"  LABEL")
 
-#We change the background color of the external part
-figure_1.patch.set_facecolor( (0.2,1,1) )
+# We change the background color of the external part
+figure_1.patch.set_facecolor((0.2, 1, 1))
 
-#--->SECOND FIGURE... 
+# --->SECOND FIGURE...
 figure_2 = plt.figure(2)
-axes_2 = figure_2.add_axes([0.2,0.2,0.6,0.6])
+axes_2 = figure_2.add_axes([0.2, 0.2, 0.6, 0.6])
 
-axes_2.plot( x_1, -y_1, c='r', linewidth=4)
+axes_2.plot(x_1, -y_1, c='r', linewidth=4)
 axes_2.set_title("MY COOL TITLE FOR FIGURE 2")
 axes_2.set_xlabel("MY  \"X\"  LABEL")
 axes_2.set_ylabel("MY  \"Y\"  LABEL")
@@ -49,5 +49,5 @@ axes_2.set_ylabel("MY  \"Y\"  LABEL")
 figure_2.patch.set_facecolor('xkcd:mint green')
 
 
-#We display the created figures of this script
+# We display the created figures of this script
 plt.show()
--- 00_LIBRARIES/02_MATPLOTLIB/02_matplotlib.py
+++ 00_LIBRARIES/02_MATPLOTLIB/02_matplotlib.py
@@ -1,83 +1,83 @@
-#WORKING WITH SUBPLOTS IN A SINGLE FIGURE
-#Santiago Garcia Arango, July 2020
+# WORKING WITH SUBPLOTS IN A SINGLE FIGURE
+# Santiago Garcia Arango, July 2020
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-#We create the vectors for the plots
-x_1 = np.linspace(0,5,1024)
+# We create the vectors for the plots
+x_1 = np.linspace(0, 5, 1024)
 y_1 = x_1**2
-y_2 = 25*np.cos( x_1 )
+y_2 = 25*np.cos(x_1)
 
-#-------------------CREATING FIGURES WITH SUBPLOTS--------------------
-#For creating multiple sub-plots in a figure, we use "plt.subplots(N,M)"
-fig, axes = plt.subplots( nrows=1, ncols=2)
+# -------------------CREATING FIGURES WITH SUBPLOTS--------------------
+# For creating multiple sub-plots in a figure, we use "plt.subplots(N,M)"
+fig, axes = plt.subplots(nrows=1, ncols=2)
 
-#Now, "fig" and "axes" are arrays, to handle the information easier
-#Note: "len(axes)" gives us the amount of objects "axes" created
-#we iterate through them, to add the titles and their properties.
-#Remark: we could do this manually (axes[0]..., axes[1]..., etc)
-for current_ax in range( len(axes) ):
-    axes[current_ax].set_title("TITLE {}".format(current_ax) )
+# Now, "fig" and "axes" are arrays, to handle the information easier
+# Note: "len(axes)" gives us the amount of objects "axes" created
+# we iterate through them, to add the titles and their properties.
+# Remark: we could do this manually (axes[0]..., axes[1]..., etc)
+for current_ax in range(len(axes)):
+    axes[current_ax].set_title("TITLE {}".format(current_ax))
     axes[current_ax].set_xlabel("MY  X  LABEL")
     axes[current_ax].set_ylabel("MY  Y  LABEL")
 
-#Now we add the plot vectors to the figure axes
-axes[0].plot( x_1, y_1 )
-axes[1].plot( x_1, -y_1 )
+# Now we add the plot vectors to the figure axes
+axes[0].plot(x_1, y_1)
+axes[1].plot(x_1, -y_1)
 
 
+# -------------------CREATE FIGURES WITH SIZE AND DPI------------------
+# We can now change the size and "resolution" of the figures...
+#   figsize --> change the actual size of the figure
+#   dpi     --> "dots per inch" (similar to resolution)
 
-#-------------------CREATE FIGURES WITH SIZE AND DPI------------------
-#We can now change the size and "resolution" of the figures...
-#  figsize --> change the actual size of the figure
-#  dpi     --> "dots per inch" (similar to resolution)
+# 1. Create figure with size and dpi specifications (may need to adjust)
+fig = plt.figure(figsize=(8, 4), dpi=180)
 
-#1. Create figure with size and dpi specifications (may need to adjust)
-fig = plt.figure( figsize=(8,4), dpi=180 )
+# 2. Create the axes based on the proportions of figure-axis plot
+ax = fig.add_axes([0.12, 0.12, 0.8, 0.78])
 
-#2. Create the axes based on the proportions of figure-axis plot
-ax = fig.add_axes([0.12,0.12,0.8,0.78])
-
-#3. Create the actual plot(s) based on the vectors
+# 3. Create the actual plot(s) based on the vectors
 #   Note:"labels" are added when we have different plots and need legends
-ax.plot( x_1, y_1, label="System 1", c="purple", linewidth=3)
-ax.plot( x_1, y_2, label="System 2", c="green", linewidth=3 )
+ax.plot(x_1, y_1, label="System 1", c="purple", linewidth=3)
+ax.plot(x_1, y_2, label="System 2", c="green", linewidth=3)
 
-#4. Add title and labels to axis of the plot
+# 4. Add title and labels to axis of the plot
 ax.set_title("FIRST HD SANTI'S PLOT", color="blue", fontsize=20)
 ax.set_xlabel("Time [s]")
 ax.set_ylabel("Voltage [V]")
 
-#5. Add legends to the plot (optional)... must add labels in step #3
+# 5. Add legends to the plot (optional)... must add labels in step #3
 #   Note: we can change location, based on another parameter "loc"
 ax.legend()
 
-#6. Change the background-color of the external part of plot
-fig.patch.set_facecolor( (0.2,1,1) )
+# 6. Change the background-color of the external part of plot
+fig.patch.set_facecolor((0.2, 1, 1))
 
 
+# ------------------------------SAVE FIGURES------------------------------
+# Current folder obtained with "os" library (to locate files better)
+current_folder = os.path.dirname(__file__)
 
-#------------------------------SAVE FIGURES------------------------------
-#Current folder obtained with "os" library (to locate files better)
-current_folder = os.path.dirname( __file__ ) 
-
-#Get the path to the location where we want to save output
+# Get the path to the location where we want to save output
 path_to_output_folder = os.path.join(current_folder, 'output')
 
-#In this case, I create the folder to save the output (more clean work)
-if not os.path.exists( path_to_output_folder ):
-    os.makedirs( path_to_output_folder )
+# In this case, I create the folder to save the output (more clean work)
+if not os.path.exists(path_to_output_folder):
+    os.makedirs(path_to_output_folder)
 
-#Save the figure in the path indicated (extension is detected automatically)
-fig.savefig( path_to_output_folder + "\\figure_1.pdf", dpi=180, facecolor = (0.2,1,1))
+# Save the figure in the path indicated (extension is detected automatically)
+fig.savefig(
+    path_to_output_folder + "\\figure_1.pdf",
+    dpi=180,
+    facecolor=(0.2, 1, 1)
+    )
 
-
-
-#--------------------------FIX SIZES CORRECTLY----------------------------
-#Remark: If we want to "shrink" the layouts to fit, we call "plt.tight_layout()"
+# --------------------------FIX SIZES CORRECTLY----------------------------
+# Remark: to "shrink" the layouts to fit, we call "plt.tight_layout()"
 plt.tight_layout()
 
-#We show and plot everything on the screen
+# We show and plot everything on the screen
 plt.show()
