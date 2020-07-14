b2a0a9748a0c6d1653d989026a1d1395b5cd0e06
san99tiago <san99tiago@gmail.com>
2020-07-14 03:18:35 -0500

    Updated <print_tree> for selecting levels to show

--- 01_DATA_STRUCTURES/01_trees/general_tree.py
+++ 01_DATA_STRUCTURES/01_trees/general_tree.py
@@ -1,6 +1,28 @@
 # GENERAL TREE STRUCTURE SETUP
 # Santiago Garcia Arango, July 2020
+"""
+The goal is to create a tree structure to organize better the information.
+The final result should be able to create a tree and print it like this:
+(from the "root", be able to go deep in a tree structure)
 
+UNIVERITY (level 0)
+    |__MANAGEMENT (level 1)
+    |   |__BUSSINESS (level 2)
+    |   |__ECONOMICS
+    |   |__RISKS
+    |__MECHATRONICS
+    |   |__ROBOTICS
+    |   |__CONTROL
+    |   |__ELECTRONICS
+    |__MECHANICS
+    |   |__THERMODYNAMICS
+    |   |__MECHANISMS
+    |   |__WELDING
+    |__BIOMEDICS
+    |   |__SIGNALS
+    |   |__BIOMATERIALS
+    |   |__BIOLOGY
+"""
 
 class TreeNode:
     """
@@ -19,13 +41,29 @@ class TreeNode:
         child.parent = self
         self.children.append(child)
 
-    def print_tree(self):
+    def get_level(self):
+        # The idea of this method is to be able to se de "depth" of this...
+        # ... specific node in the tree, to use it the print_tree() method
+        level = 0
+        current_parent = self.parent
+        while current_parent != None:
+            # Add levels by recursive accessing each parent (until None)
+            level += 1
+            current_parent = current_parent.parent
+        return level
+
+    def print_tree(self, levels_to_show=5):
+        # Extra printing setup for organizing visual tree
+        setup = "    |" * self.get_level() + "__"
+
         # Access the information of this specific TreeNode
-        print(self.data)
+        print(setup + self.data)
 
         if len(self.children) > 0:
             # For each child, do a recursive approach of <print_tree> method
             for c in self.children:
-                c.print_tree()
-                
+                # Extra condition to only show levels given by the user
+                if self.get_level() < levels_to_show:
+                    c.print_tree(levels_to_show)
+
 # Tests are in < test_general_tree.py >
--- 01_DATA_STRUCTURES/01_trees/test_general_tree.py
+++ 01_DATA_STRUCTURES/01_trees/test_general_tree.py
@@ -3,38 +3,99 @@
 
 import general_tree
 
-# Create main root
+# ----------------------------CREATE NODES--------------------------------
+# Create main root (LEVEL 0)
 root = general_tree.TreeNode("UNIVERSITY EIA")
 
-# Create nodes for the first children
-management = general_tree.TreeNode("MANAGEMENT")
-mechatronics = general_tree.TreeNode("MECHATRONICS")
-mechanics = general_tree.TreeNode("MECHANICS")
-biomedics = general_tree.TreeNode("BIOMEDICS")
+# Create nodes (LEVEL 1)
+a1 = general_tree.TreeNode("MECHATRONICS")
+b1 = general_tree.TreeNode("MANAGEMENT")
+c1 = general_tree.TreeNode("MECHANICS")
+d1 = general_tree.TreeNode("BIOMEDICS")
 
-#Create the second children from the first ones
-management.add_child(general_tree.TreeNode("BUSSINESS"))
-management.add_child(general_tree.TreeNode("ECONOMICS"))
-management.add_child(general_tree.TreeNode("RISKS"))
+# Create nodes (LEVEL 2)
+a11 = general_tree.TreeNode("ROBOTICS")
+a12 = general_tree.TreeNode("CONTROL")
+a13 = general_tree.TreeNode("ELECTRONICS")
 
-mechatronics.add_child(general_tree.TreeNode("ROBOTICS"))
-mechatronics.add_child(general_tree.TreeNode("CONTROL"))
-mechatronics.add_child(general_tree.TreeNode("SIGNALS"))
+b11 = general_tree.TreeNode("BUSSINESS")
+b12 = general_tree.TreeNode("ECONOMICS")
+b13 = general_tree.TreeNode("RISKS")
 
-mechanics.add_child(general_tree.TreeNode("THERMODYNAMICS"))
-mechanics.add_child(general_tree.TreeNode("MECHANISMS"))
-mechanics.add_child(general_tree.TreeNode("WELDING"))
+c11 = general_tree.TreeNode("THERMODYNAMICS")
+c12 = general_tree.TreeNode("MECHANISMS")
+c13 = general_tree.TreeNode("WELDING")
 
-biomedics.add_child(general_tree.TreeNode("HISTOLOGY"))
-biomedics.add_child(general_tree.TreeNode("BIOMATERIALS"))
-biomedics.add_child(general_tree.TreeNode("BIOLOGY"))
+d11 = general_tree.TreeNode("BIOSIGNALS")
+d12 = general_tree.TreeNode("BIOMATERIALS")
+d13 = general_tree.TreeNode("ANATOMY")
+
+# Create nodes (LEVEL 3) ... only a few
+a111 = general_tree.TreeNode("prof: Juan Camilo Tejada")
+a112 = general_tree.TreeNode("prof: Gustavo Moreno")
+
+a121 = general_tree.TreeNode("prof: Dolly Tatiana")
+a122 = general_tree.TreeNode("prof: Sebastian Jimenez")
+
+a131 = general_tree.TreeNode("prof: Yeison Javier Montagut")
+a132 = general_tree.TreeNode("prof: Camilo Buritica")
+a133 = general_tree.TreeNode("prof: Dario Jaramillo")
+
+b121 = general_tree.TreeNode("prof: Elmer Algo")
+b122 = general_tree.TreeNode("prof: Robert Ng")
+
+c131 = general_tree.TreeNode("prof: Elizabeth Hoyos")
+c132 = general_tree.TreeNode("prof: Luis V. Wilches")
+
+# ------------------------ADD NODES RELATIONSHIPS-------------------------
+# Add children (LEVEL 2-->3)
+a11.add_child(a111)
+a11.add_child(a112)
+
+a12.add_child(a121)
+a12.add_child(a122)
+
+a13.add_child(a131)
+a13.add_child(a132)
+a13.add_child(a133)
+
+b12.add_child(b121)
+b12.add_child(b122)
+
+c13.add_child(c131)
+c13.add_child(c132)
 
 
-# Add first children children
-root.add_child(management)
-root.add_child(mechatronics)
-root.add_child(mechanics)
-root.add_child(biomedics)
+# Add children (LEVEL 1-->2)
+a1.add_child(a11)
+a1.add_child(a12)
+a1.add_child(a13)
+
+b1.add_child(b11)
+b1.add_child(b12)
+b1.add_child(b13)
+
+c1.add_child(c11)
+c1.add_child(c12)
+c1.add_child(c13)
+
+d1.add_child(d11)
+d1.add_child(d12)
+d1.add_child(d13)
+
+# Add children (LEVEL 0-->1)
+root.add_child(a1)
+root.add_child(b1)
+root.add_child(c1)
+root.add_child(d1)
 
 
-root.print_tree()
+# ---------------------PRINTING TREE BY LEVELS-----------------------------
+print("\n---------TREE LEVEL 0:-----------")
+root.print_tree(0)
+print("\n---------TREE LEVEL 1:-----------")
+root.print_tree(1)
+print("\n---------TREE LEVEL 2:-----------")
+root.print_tree(2)
+print("\n---------TREE LEVEL 3:-----------")
+root.print_tree(3)
