ed2926013824b0516e38bd5e19cb34f511054543
san99tiago <san99tiago@gmail.com>
2020-07-19 03:39:52 -0500

    Added classes to show and test graph structure

--- 01_DATA_STRUCTURES/02_graphs/graph.py
+++ 01_DATA_STRUCTURES/02_graphs/graph.py
@@ -9,6 +9,44 @@ class Vertex:
     def add_neighbor(self, vertex_neighbor):
         # Check that the neighbor vertex is NOT already in neighbors
         if vertex_neighbor not in self.neighbors:
-            self.neighbors.apprend(vertex_neighbor)
+            self.neighbors.append(vertex_neighbor)
+            # We sort the objects by their name attribute
+            self.neighbors.sort(key=lambda x: x.name)
+            # self.neighbors.sort()
 
+    def show_neighbors(self):
+        print("[", end="")
+        for neighbor in self.neighbors:
+            print(neighbor.name, end=",")
+        print("]")
 
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    
+    def add_vertex(self, new_vertex):
+        is_vertex = isinstance(new_vertex, Vertex)
+        is_not_in_graph = new_vertex.name not in self.vertices
+        if is_vertex and is_not_in_graph:
+            self.vertices[new_vertex.name] = new_vertex
+            return True
+        else:
+            return False
+
+    def add_edge(self, vertex_1, vertex_2):
+        name_1 = vertex_1.name
+        name_2 = vertex_2.name
+        are_in_graph = name_1 in self.vertices and name_2 in self.vertices
+        if are_in_graph:
+            self.vertices[name_1].add_neighbor(vertex_2)
+            self.vertices[name_2].add_neighbor(vertex_1)
+            return True
+        else:
+            return False
+
+    def show_graph(self):
+        for key in self.vertices.keys():
+            print(key + " --> ", end="")
+            self.vertices[key].show_neighbors()
+
+# Check test in <test_graph.py> script
---
+++ 01_DATA_STRUCTURES/02_graphs/test_graph.py
@@ -0,0 +1,29 @@
+# TEST GRAPH SCRIPT WITH SIMPLE EXAMPLE
+# Santiago Garcia Arango, July 2020
+
+import graph
+
+# Create Graph object
+g1 = graph.Graph()
+
+# Create all Vertex objects (that will be added to Graph)
+S = graph.Vertex("S")
+A = graph.Vertex("A")
+B = graph.Vertex("B")
+C = graph.Vertex("C")
+D = graph.Vertex("D")
+E = graph.Vertex("E")
+G = graph.Vertex("G")
+
+# Add all Vertex objects to the graph
+g1.add_vertex(S)
+g1.add_vertex(A)
+g1.add_vertex(B)
+g1.add_vertex(C)
+g1.add_vertex(D)
+g1.add_vertex(E)
+g1.add_vertex(G)
+
+g1.add_edge(S, A)
+g1.add_edge(S, B)
+g1.show_graph()
