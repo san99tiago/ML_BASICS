Fixed dimensional problem of array in gradient of cost function

02_ML_MODELS/00_LINEAR_REGRESSION/batch_gradient_descent.py
@@ -1,27 +1,27 @@
 # BATCH GRADIENT DESCENT ALGORITHM
-# Santiago Garcia Arango, JulY 2020
+# Santiago Garcia Arango, July 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 class GradientDescent:
-    def __init__(self, alpha, X, Y, ep=0.0001, max_iterations=10000):
+    def __init__(self, alpha, X, y, ep=0.0001, max_iterations=10000):
         """
         Gradient descent algorithm with "batch approach" for finding a
         vector theta with the parameters of a linear regression solution.
         :param alpha: constant for the learning rate.
         :param X: vector of features (it must have shape (M, N)), where M
         is the number of training examples and N is the number of features.
-        :param Y: vector of target variables (outputs)
+        :param y: vector of target variables (outputs)
         :param ep: constant value for eps of algorithm.
         :param max_iterations: constant integer of maximum iterations.
         """
 
-        self.X = X
-        self.Y = Y
+        self.X = X  # X.shape = (M, N), but will become (M, N+1)
+        self.y = y  # y.shape = (M, 1)
         m = self.X.shape[0]  # Number of training examples
-        n = self.X.shape[1] + 1  # Number of features (X inputs)
+        n = self.X.shape[1]  # Number of features (X inputs)
         self.create_initial_theta(n)
         self.add_1_bias_to_X()
         self.main_loop(alpha, n, m, ep, max_iterations)
@@ -29,20 +29,18 @@ class GradientDescent:
     def create_initial_theta(self, n):
         # n: the number of features given in X vector (like X inputs)
         theta = []
-        for i in range(n):
+        for i in range(n+1):
             theta.append(np.random.random())
         self.theta = np.array(theta)
 
-        self.t0 = theta[0]
-        self.t1 = theta[1]
-
     def add_1_bias_to_X(self):
+        # Add extra column of "1" bias to self.X vector...
         self.X = np.c_[np.ones((len(self.X), 1)), self.X]
 
     def get_cost_function_J(self, m):
         # Cost function, J(theta)
         predictions = np.dot(self.X, self.theta)
-        J = (1/2*m) * np.sum(np.square(predictions - self.Y))
+        J = (1/2*m) * np.sum(np.square(predictions - self.y))
         return J
 
     def main_loop(self, alpha, n, m, ep, max_iterations):
@@ -50,11 +48,16 @@ class GradientDescent:
         J = self.get_cost_function_J(m)
         iter = 0
         while not converged:
+            # Some dimensional analysis to make sure we are ok:
+            # -->self.y.shape = (M, 1)
+            # -->predictions.shape = (M, 1) (also known as hypothesis)
+            # -->self.X.T.shape = (N+1, M)
+            # -->gradient_vector.size = (N+1, 1)
 
             predictions = np.dot(self.X, self.theta)
-            gradient_vector = np.dot((predictions - self.Y), self.X)
-            for j in range(n):
-                self.theta[j] = self.theta[j] - (alpha/m) * gradient_vector[j]
+            gradient_vector = np.dot(self.X.T, (predictions - self.y))
+
+            self.theta = self.theta - (alpha/m) * gradient_vector
 
             J_new = self.get_cost_function_J(m)
 
@@ -70,7 +73,6 @@ class GradientDescent:
                 converged = True
 
     def get_thetas(self):
-        # return self.t0, self.t1
         return self.theta
 
     def plot_result(self):
@@ -80,12 +82,12 @@ class GradientDescent:
         if features == 2:
             # Plot the training examples and the linear regression result
             for i in range(self.X.shape[0]):
-                Y_predict = self.theta[0] + self.theta[1]*self.X[:, 1]
+                y_predict = self.theta[0] + self.theta[1]*self.X[:, 1]
 
             # Create plot based on matplotlib to show linear regression
             plt.figure()
-            plt.plot(self.X[:, 1], self.Y, 'o')
-            plt.plot(self.X[:, 1], Y_predict, 'k')
+            plt.plot(self.X[:, 1], self.y, 'o')
+            plt.plot(self.X[:, 1], y_predict, 'k')
             plt.show()
 
 