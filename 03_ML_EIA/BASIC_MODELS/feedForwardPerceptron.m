bc959b4f87d4ab7de8cc9d71c637ad9ce2876dcd
Santiago <santiago.garcia1999@hotmail.com>
2020-08-15 18:01:13 -0500

    Added feedForwardPerceptron description and added possibiliy to select activation_function

--- 03_ML_EIA/BASIC_MODELS/feedForwardPerceptron.m
+++ 03_ML_EIA/BASIC_MODELS/feedForwardPerceptron.m
@@ -1,9 +1,21 @@
 % TRAIN OR TEST DATA BASED ON FEED-FORWARD-PERCEPTRON
 % Santiago Garcia Arango
+% ------------------------------------------------------------------------
+% FEEDFORWARDPERCEPTRON. Train or Test a simple network.
+%   alfa: Learning rate constante (1 x 1)
+%   
+%   entries: input data based on the "Xi" values (ne x nd)
+%   
+%   w: vector of the initial weights. size (1 x ne)
+%   
+%   goal: "train" to change the weights, "test" to check other values
+%   
+%   activation_function: "none" to pass "ys" direct.
+%                        "binary" to apply ">=0" or "<0" filter.
 
-function [yrk,ecm, w] = feedForwardPerceptron(alfa, entries, w, desired, goal)
-%FEEDFORWARDPERCEPTRON Summary of this function goes here
-%   Train or Test a simple neural network based on shown parameters
+
+function [yrk,ecm, w] = feedForwardPerceptron(alfa, entries, w, desired, goal, activation_function)
+
     nd = size(entries, 2);  % Number of Data
     ns = size(desired, 1);  % Number of Outputs
 
@@ -14,17 +26,31 @@ function [yrk,ecm, w] = feedForwardPerceptron(alfa, entries, w, desired, goal)
     for i=1:nd
         % Main sensation output (before activation function)
         ys = w*entries(:,i);
+        disp("ys:");
+        disp(ys);
         
-        % Apply activation function
-        yrk(i) = (ys>=0);  % Matlab built-in function (1 or 0)
+        % Apply activation function based on funciton parameters
+        if activation_function == "none"
+            yrk(i) = ys;
+        end
+        if activation_function == "binary"
+            yrk(i) = (ys>=0);  % Matlab built-in function (1 or 0)
+        end
+
         
         % Simple error and cuadratic mean error
         error = desired(i) - yrk(i);
+        
         ecm(:) = ecm(:) + (error.^2)/2;
         
         if goal == "train"
             % Update weights
             w = w + alfa.*(error*entries(:,i)');
+            
+            disp("alfa.*(error*entries(:,i)'):")
+            disp(alfa.*(error*entries(:,i)'))
+            disp("w:")
+            disp(w)
         end        
     end
     
