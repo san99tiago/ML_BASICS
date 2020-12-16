0edf46c1707f7c1399114171a7161742ef5672f6
Santiago <santiago.garcia1999@hotmail.com>
2020-12-15 22:59:57 -0500

    Added template for layers_of_neurons.py for multiple layers of neurons

---
+++ 04_NEURAL_NETWORKS/cap_03_adding_layers/layers_of_neurons.py
@@ -0,0 +1,31 @@
+# CODE FOR A MULTIPLE LAYER OF NEURONS USING NUMPY LIBRARY
+# SANTIAGO GARCIA ARANGO
+
+import numpy as np
+
+
+def general_layer_of_neurons():
+    """
+    general_layer_of_neurons is a simple function to show a single layer of
+    neurons using NumPy library to simplify code and get more efficient results
+    """
+    inputs = [[1.0, 2.0, 3.0, 2.5],
+            [2.0, 5.0, -1.0, 2.0],
+            [-1.5, 2.7, 3.3, -0.8]]
+
+    weights = [[0.2, 0.8, -0.5, 1],
+            [0.5, -0.91, 0.26, -0.5],
+            [-0.26, -0.27, 0.17, 0.87]]
+
+    biases = [2.0, 3.0, 0.5]
+
+    layer_outputs = np.dot(inputs, np.array(weights).T) + biases
+
+    outputs = layer_outputs
+
+    print("\nOUTPUTS 2:\n{}".format(outputs))
+
+
+if __name__ == "__main__":
+    simple_layer_of_neurons()
+    general_layer_of_neurons()
