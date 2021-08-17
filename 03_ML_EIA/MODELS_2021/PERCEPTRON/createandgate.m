6212721ceea81b1bf08c247fcfd83f28fbf70b53
Santiago Garcia Arango <san99tiago@gmail.com>
2021-08-17 10:40:04 -0500

    Delete directory with capital letters convention

--- 03_ML_EIA/MODELS_2021/PERCEPTRON/createandgate.m
+++
@@ -1,8 +0,0 @@
-% SIMPLE SCRIPT TO CREATE "AND-GATE" WORKSPACE AND SAVE IT TO "AND.mat"
-% Santiago Garcia Arango
-
-entries = [0, 1, 0, 1; 0, 0, 1, 1];
-desired = [0, 0, 0, 1];
-
-% Automatically save the data to the "AND.mat" workspace
-save("AND.mat", "entries", "desired");
